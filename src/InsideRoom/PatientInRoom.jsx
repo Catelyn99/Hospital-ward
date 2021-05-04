c58d93f46b6df5676b9fbcd3927ca2aff3b59081
Catelyn99 <katarzynasroka99@gmail.com>
2021-05-04 09:14:21 +0200

    some change in patient scss

--- src/InsideRoom/PatientInRoom.jsx
+++ src/InsideRoom/PatientInRoom.jsx
@@ -5,12 +5,13 @@ import styled from './PatientInRoom.module.scss';
 const PatientInRoom = (props) => {
     const patients = useContext(PatientsContext);
     return (
-
-        <div onClick={() => patients.showPatientInfo(props.id)}>
-            <div className={styled.patientBed}>
-                <p className={styled.p}>Pacjent: {props.name}</p>
-                <p className={styled.p}>Wiek: {props.age}</p>
-            </div>
+        <div className={styled.patientBed} onClick={() => patients.showPatientInfo(props.id)}>
+                {
+                    props.name !== null ? 
+                <> <p className={styled.bedWords}>Pacjent: {props.name}</p>
+                <p className={styled.bedWords}>Wiek: {props.age}</p> </> :
+                 <p className={styled.bedWords}>Puste łóżko</p>
+                }
         </div>
     )
     }
